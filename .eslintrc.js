eefdf7ceb4636b01e4fb6f9cd40bf990cfe9932f
keyto1995 <keyto1995@outlook.com>
2020-10-18 11:10:38 +0800

    配置格式定义;

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,9 +7,10 @@ module.exports = {
   parserOptions: {
     parser: "babel-eslint",
   },
+  // https://eslint.org/docs/rules/
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "comma-dangle": ["error", "always-multiline"],
+    "comma-dangle": ["error", "only-multiline"],
   },
 };
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,8 @@
+// https://prettier.io/docs/en/options.html
 module.exports = {
   trailingComma: "es5",
-  tabWidth: 2,
+  // tabWidth: 2,
   semi: true,
-  singleQuote: false,
-}
+  // singleQuote: false,
+  arrowParens: "always",
+};
